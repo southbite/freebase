e974fa41b06ba4a3092b457f8ab77cb44aa36d82
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2014-05-29 13:55:08 +0200

    Cleaned up, added collection delete functionality

--- test/e2e_test.js
+++ test/e2e_test.js
@@ -13,6 +13,12 @@ describe('e2e test', function() {
 	var testport = 8000;
 	var test_secret = 'test_secret';
 
+	/*
+	This test demonstrates starting up the freebase service - with 5 worker processes
+	the authentication service will use authTokenSecret to encrypt web tokens identifying
+	the logon session. The utils setting will set the system to log non priority information
+	*/
+
 	it('should initialize the service', function(callback) {
 		
 		this.timeout(20000);
@@ -21,7 +27,8 @@ describe('e2e test', function() {
 
 			service.initialize({size:5, port:testport, services:{
 				auth:{authTokenSecret:'a256a2fd43bf441483c5177fc85fd9d3',
-				systemSecret:test_secret}
+				systemSecret:test_secret},
+				utils:{log_level:'info|error|warning'}
 			}}, function(e){
 				callback(e);
 			});
@@ -34,6 +41,10 @@ describe('e2e test', function() {
 	var publisherclient;
 	var listenerclient;
 
+	/*
+	We are initializing 2 clients to test saving data against the database, one client will push data into the 
+	database whilst another listens for changes.
+	*/
 
 	it('should initialize the clients', function(callback) {
 		
@@ -65,6 +76,9 @@ describe('e2e test', function() {
 	});
 
 
+	/*
+	We are testing setting data at a specific path
+	*/
 
 	it('the publisher should set data ', function(callback) {
 		
@@ -73,10 +87,6 @@ describe('e2e test', function() {
 		try{
 
 			publisherclient.put('e2e_test1/testsubscribe/data', {property1:'property1',property2:'property2',property3:'property3'}, function(e, result){
-
-				console.log('put happened');
-				console.log(e);
-				console.log(result);
 			
 				if (!e){
 					publisherclient.get('e2e_test1/testsubscribe/data', function(e, results){
@@ -92,6 +102,10 @@ describe('e2e test', function() {
 		}
 	});
 
+	/*
+	We are testing pushing a specific value to a path which will actually become an array in the database
+	*/
+
 	it('the publisher should push to a collection and get a child', function(callback) {
 		
 		this.timeout(10000);
@@ -100,18 +114,9 @@ describe('e2e test', function() {
 
 				publisherclient.post('e2e_test1/testsubscribe/data/collection', {property1:'post_property1',property2:'post_property2'}, function(e, results){
 
-					console.log('POST RESULTS');
-					console.log(results);
-
 					if (!e){
-
+						//the child method returns a child in the collection with a specified id
 						publisherclient.child('e2e_test1/testsubscribe/data/collection', results.data._id, function(e, results){
-
-							if (!e){
-								console.log('GOT CHILD');
-								console.log(results);
-							}
-
 							callback(e);
 						});
 
@@ -126,18 +131,20 @@ describe('e2e test', function() {
 		}
 	});
 
+	/*
+	We set the listener client to listen for a PUT event according to a path, then we set a value with the
+	publisher client.
+	*/
 	it('the listener should pick up a single published event', function(callback) {
 		
 		this.timeout(10000);
 
 		try{
 
+			//first listen for the change
 			listenerclient.on('/e2e_test1/testsubscribe/data/event', 'PUT', 1, function(e, message){
 
-				console.log('on happened');
-				console.log(message);
 				expect(listenerclient.events['/PUT@/e2e_test1/testsubscribe/data/event'].length).to.be(0);
-
 				callback(e);
 
 			}, function(e){
@@ -150,13 +157,119 @@ describe('e2e test', function() {
 
 					console.log('on subscribed, about to publish');
 
+					//then make the change
 					publisherclient.put('/e2e_test1/testsubscribe/data/event', {property1:'property1',property2:'property2',property3:'property3'}, function(e, result){
-						console.log('put happened');
-						console.log(result);
+						console.log('put happened - listening for result');
 					});
 				}else
 					callback(e);
 			});
+
+		}catch(e){
+			callback(e);
+		}
+	});
+
+	/*
+	We are testing the deletion of data at a set path, and listening for the DELETE event at that path.
+	*/
+	it('the listener should pick up a single delete event', function(callback) {
+		
+		this.timeout(10000);
+
+		try{
+
+				//We put the data we want to delete into the database
+				publisherclient.put('/e2e_test1/testsubscribe/data/delete_me', {property1:'property1',property2:'property2',property3:'property3'}, function(e, result){
+
+					//We listen for the DELETE event
+					listenerclient.on('/e2e_test1/testsubscribe/data/delete_me', 'DELETE', 1, function(e, message){
+
+						//we are looking at the event internals on the listener to ensure our event management is working - because we are only listening for 1
+						//instance of this event - the event listener should have been removed 
+						expect(listenerclient.events['/DELETE@/e2e_test1/testsubscribe/data/delete_me'].length).to.be(0);
+
+						//we needed to have removed a single item
+						expect(message.removed).to.be(1);
+
+						console.log(message);
+
+						callback(e);
+
+					}, function(e){
+
+						console.log('ON HAS HAPPENED: ' + e);
+
+						if (!e){
+
+							expect(listenerclient.events['/DELETE@/e2e_test1/testsubscribe/data/delete_me'].length).to.be(1);
+
+							console.log('subscribed, about to delete');
+
+							//We perform the actual delete
+							publisherclient.delete('/e2e_test1/testsubscribe/data/delete_me', null, function(e, result){
+								console.log('put happened - listening for result');
+							});
+						}else
+							callback(e);
+					});
+				});
+
+			
+
+		}catch(e){
+			callback(e);
+		}
+	});
+
+	it('should delete a child from an array', function(callback) {
+		
+		this.timeout(10000);
+
+		try{
+
+				publisherclient.post('/e2e_test1/testsubscribe/data/arr_delete_me', {property1:'property1',property2:'property2',property3:'property3'}, function(e, post_result){
+
+					if (e)
+						return callback(e);
+
+					publisherclient.get('/e2e_test1/testsubscribe/data/arr_delete_me', function(e, results){
+
+						if (e)
+							return callback(e);
+
+						console.log('got array');
+						console.log(results);
+
+						
+						expect(results.data.length).to.be(1);
+
+						publisherclient.delete('/e2e_test1/testsubscribe/data/arr_delete_me', post_result.data._id, function(e, delete_result){
+
+							if (e)
+							return callback(e);
+
+							console.log('delete happened');
+							console.log(delete_result);
+
+							publisherclient.get('/e2e_test1/testsubscribe/data/arr_delete_me', function(e, results){
+
+								if (e)
+									return callback(e);
+
+								expect(results.data.length).to.be(0);
+
+								callback(e);
+
+							});
+
+							
+						});
+
+
+					});
+
+				});
 
 		}catch(e){
 			callback(e);
