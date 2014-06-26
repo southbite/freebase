43a5770cc0527422699a48807ab129c3a82cfd7f
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2014-06-26 14:58:54 +0200

    updated catchall event to emit payload

--- test/e2e_test.js
+++ test/e2e_test.js
@@ -87,6 +87,8 @@ describe('e2e test', function() {
 			if (!caught[message.action])
 				caught[message.action] = 0;
 
+			console.log('ONALL ' + message);
+
 			console.log(message);
 
 			caught[message.action]++;
